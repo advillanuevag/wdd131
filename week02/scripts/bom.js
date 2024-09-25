e18f48b4afe2837c713ecc45881c18bcb4372455
advillanuevag <vil24014@byui.edu>
2024-09-24 23:39:46 -0300

    Book OF Mormon App Basic Interface

---
+++ week02/scripts/bom.js
@@ -0,0 +1,10 @@
+const list = document.querySelector('ul');
+const input = document.querySelector('favchap');
+const button = document.querySelector('button');
+
+const listItem = document.createElement("li");
+const deleteButton = document.createElement("button");
+li.textContent = input.value;
+deleteButton.textContent = "❌";
+listItem.append(deleteButton);
+list.append(li);
