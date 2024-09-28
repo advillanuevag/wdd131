e3035c42b71b2d88f84ac557e753171ebfc2427f
advillanuevag <vil24014@byui.edu>
2024-09-27 22:05:17 -0300

    Changes in JS files

--- scripts/temples.js
+++ scripts/temples.js
@@ -8,3 +8,25 @@ hambutton.addEventListener('click', () => {
 	hambutton.classList.toggle('show');
 });
 
+document.addEventListener('DOMContentLoaded', () => {
+    const navOptions = document.querySelectorAll('.nav-option');
+    const activeOption = document.getElementById('active-nav-option');
+
+    navOptions.forEach(option => {
+        option.addEventListener('click', () => {
+            navOptions.forEach(option => option.classList.remove('active'));
+            option.classList.add('active');
+            activeOption.textContent = option.textContent;
+        });
+    });
+});
+
+const $year = document.querySelector("#currentyear");
+
+const $lastModified = document.querySelector("#lastModified");
+
+const today = new Date();
+
+$year.innerHTML = today.getFullYear();
+$lastModified.innerHTML = `Last Modification: ${document.lastModified}`;
+
