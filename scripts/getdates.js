6953093e9fe24e2cf3a8e8f63881cc47987e2a34
advillanuevag <vil24014@byui.edu>
2024-09-27 21:39:41 -0300

    Debug Log

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -6,3 +6,16 @@ const today = new Date();
 
 $year.innerHTML = today.getFullYear();
 $lastModified.innerHTML = `Last Modification: ${document.lastModified}`;
+
+document.addEventListener('DOMContentLoaded', () => {
+    const navOptions = document.querySelectorAll('.nav-option');
+    const activeOption = document.getElementById('active-nav-option');
+
+    navOptions.forEach(option => {
+        option.addEventListener('click', () => {
+            navOptions.forEach(option => option.classList.remove('active'));
+            option.classList.add('active');
+            activeOption.textContent = option.textContent;
+        });
+    });
+});
