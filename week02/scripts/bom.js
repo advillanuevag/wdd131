37e298ff60c0e3c793ff20ec4a38f403d9aad01c
advillanuevag <vil24014@byui.edu>
2024-11-14 22:17:37 -0300

    Week02 bom code

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -1,29 +1,28 @@
-document.addEventListener('DOMContentLoaded', function(){
-    const input = document.querySelector('favchap');
+document.addEventListener('DOMContentLoaded', function () {
+    const input = document.querySelector('#favchap');
     const button = document.querySelector('button');
     const list = document.querySelector('ul');
 
-button.addEventListener('click',() => {
-        const chapter = input.value.trim();
-        if (chapter){
-            const listItem = document.createElement("li");
-            listItem.textContent = chapter;
+button.addEventListener('click', () => {
+    
+    const chapter = input.value.trim();
+    if (chapter) {
+        const listItem = document.createElement("li");
+        listItem.textContent = chapter;
 
-            const deleteButton = document.createElement("button");
-            deleteButton.textContent ="❌";
-            listItem.appendChild(deleteButton);
-            list.appendChild(listItem);
-            input.value = "";
-
-            deleteButton.addEventListener('click', () => {
-                list.removeChild(listItem);
-            });
-
-        }else{
-            alert("Please enter a Chapter");
-        }
+        const deleteButton = document.createElement("button");
         
-
-    })
-
-})
+        deleteButton.textContent = "❌";
+        listItem.appendChild(deleteButton);
+        list.appendChild(listItem);
+        input.value = "";
+        
+        deleteButton.addEventListener('click', () => {
+          list.removeChild(listItem);
+        });
+      }else{
+        alert('Please enter a chapter');
+      }
+});
+});
+          
